8b69737055d44b4ed6e2630e2ccae703ecce2297
Dimotro <dimotro@Dimotros-MacBook-Pro.local>
2021-12-02 11:38:39 +0100

    test

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 
 $host = "localhost";
 $user = "root";
-$pass = "";
-$databaseName = "cursus";
+$pass = "root";
+$databaseName = "nerdygadgets";
 $port = 3306;
 $connection = mysqli_connect($host, $user, $pass, $databaseName, $port);
 
